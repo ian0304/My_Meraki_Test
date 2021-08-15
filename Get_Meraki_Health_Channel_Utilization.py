fc91c8006c462a7bd788140779024b25c75ab316
Ian Yang <ianyoung0304@gmail.com>
2021-08-15 12:58:08 +0800

    Add URL option

--- Get_Meraki_Health_Channel_Utilization.py
+++ Get_Meraki_Health_Channel_Utilization.py
@@ -4,10 +4,23 @@ import getpass
 from matplotlib import pyplot as plt
 
 print("This script is to get Wireless Serial from Meraki Dashboard")
+url_location = input("""
+Which Meraki URL you want to access: 
+1) https://api.meraki.com
+2) https://api.meraki.cn
+
+""")
+if url_location == '1':
+    url = 'https://api.meraki.com'
+elif url_location == '2':
+    url = 'https://api.meraki.cn'
+print("Accessing {} ...".format(url))
+
 org_name = input("Enter Your Org Name: ")
 net_name = input("Enter Network Name: ")
 api_key = getpass.getpass("Enter Your API_KEY: ")
 time_range = 0-int(input("Last 1, 2 or 3 hours' Channel Utilization you want to check: "))*6
+
 wifi_channel = input("""
 Which wifi channel you want to check: 
 1) 2.4G
@@ -16,10 +29,10 @@ Which wifi channel you want to check:
 """)
 if wifi_channel == '1':
     wifi_channel = 'wifi0'
+    wifi_signal = '2.4G'
 elif wifi_channel == '2':
     wifi_channel = 'wifi1'
-
-url = 'https://api.meraki.com'
+    wifi_signal = '5G'
 
 dashboard = meraki.DashboardAPI(
     api_key=api_key,
@@ -50,8 +63,8 @@ for i in channel_utili:
         utilization.append(xy['utilization'])
     plt.figure(figsize=(15, 8))
     plt.plot(time_stamp[time_range:],utilization[time_range:]) 
-    plt.title('{} 2.4G WIFI Utilization'.format(i['serial']))
-    plt.ylabel('2.4GWIFI Utilization')
+    plt.title('{} {} WIFI Utilization'.format(i['serial'],wifi_signal))
+    plt.ylabel('{} WIFI Utilization'.format(wifi_signal))
     plt.xlabel('Time')
     plt.show()
     
